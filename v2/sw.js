52d269ae3849c82d646058279e5c003f2d276fc4
Sam-WoT <centersam19@gmail.com>
2018-05-05 16:33:23 -0400

    Get ready to be a PWA

---
+++ v2/sw.js
@@ -0,0 +1,44 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
+var CACHE_NAME = 'LongMetal-Scouting-cache-v1';
+var urlsToCache = [
+  '/',
+  '/res/main.css',
+  '/res/js/main.js',
+  'https://fonts.googleapis.com/css?family=Lato|Open+Sans|Material+Icons'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
